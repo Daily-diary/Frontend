bef447df38ed83ae751dba3b1ec6dc4f9b59adda
김석현 <kbx1498@gmail.com>
2026-06-08 18:50:12 +0900

    Revert "refactor: 라우팅/레이아웃 구조 재정리 및 구 Home 페이지 제거"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppLayout from './layouts/AppLayout';
-import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile'; 
 
 function App() {
   return (
     <BrowserRouter>
-      <AppLayout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
